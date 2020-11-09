064cd7d84ec72029400752e026c69661a7d2d900
canta2899 <56071114+canta2899@users.noreply.github.com>
2020-11-09 15:12:08 +0100

    Followers folder for serializer script

---
+++ canta_secret.py
@@ -0,0 +1,9 @@
+key = "VZYzRkLF8dOobuhSuAEB7weDs"
+
+secret = "IvEF5SiYpka9aP49yCbVp3TuGpSYYbEVum0SPqdjKGJciDqkcC"
+
+bearer = "AAAAAAAAAAAAAAAAAAAAAFsTJQEAAAAA5w2cWd9FtZIfZRGq9r8cNJZast8%3DyJMlS1L68wzrGo1HoObL3JoHFXA7D6DlKwIRtRqHUSHkWHZEEm"
+
+access_token = "1321104041217314817-s24Maxwxt0V7soszfRdAd3mVsApoiR"
+
+access_secret = "irKzfr4Obyyy7oUVX32BGOvL3p2MZ2Y8xOA7opfbI3TNh"
