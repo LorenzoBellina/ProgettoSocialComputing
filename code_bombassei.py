6351fc202df3888c5b61b66ec597b948af947091
bomb697 <50354770+bomb697@users.noreply.github.com>
2020-11-09 15:23:17 +0100

    Accesso direttamente al json di ogni follower

--- code_bombassei.py
+++ code_bombassei.py
@@ -18,9 +18,6 @@ for user in users:
     ).items():
         found_follower = item._json
 
-        if found_follower not in user_followers:
-            user_followers.append(found_follower)
-
     print(f"Found {len(user_followers)} followers for @{user}")
     serializer = se.Serializer('followers')
     serializer.serialize_json(f"{user}_followers.json", user_followers)
